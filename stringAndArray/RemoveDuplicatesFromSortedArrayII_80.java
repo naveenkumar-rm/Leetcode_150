b83a0ff7ba45c3812a242371525a998da0c44643
naveenkumar-rm <naveenkmayilsamy@gmail.com>
2026-03-14 20:28:05 +0530

    LC 80: Remove Duplicates from Sorted Array II

---
+++ stringAndArray/RemoveDuplicatesFromSortedArrayII_80.java
@@ -0,0 +1,16 @@
+package stringAndArray;
+
+public class RemoveDuplicatesFromSortedArrayII_80 {
+	public int removeDuplicates(int[] nums) {
+        if(nums.length<=2) return nums.length;
+        int count =2;
+
+        for(int i=2;i<nums.length;i++){
+            if(nums[i]!=nums[count-2]){
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
